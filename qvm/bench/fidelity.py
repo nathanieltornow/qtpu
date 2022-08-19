40ea08f5a1c3ce5db4ee2478ee25d8dac89bf96a
Nathaniel Tornow <nathaniel.tornow@tum.de>
2022-08-19 16:20:08 +0200

    move fidelity

---
+++ qvm/bench/fidelity.py
@@ -0,0 +1,16 @@
+from typing import Dict
+from qiskit import QuantumCircuit
+from qiskit.quantum_info import hellinger_fidelity
+from qiskit.providers.aer import StatevectorSimulator
+
+
+def perfect_counts(original_circuit: QuantumCircuit) -> Dict[str, int]:
+    return (
+        StatevectorSimulator().run(original_circuit, shots=20000).result().get_counts()
+    )
+
+
+def fidelity(orginal_circuit: QuantumCircuit, noisy_counts: Dict[str, int]) -> float:
+    print("n", noisy_counts)
+    print("p", perfect_counts(orginal_circuit))
+    return hellinger_fidelity(perfect_counts(orginal_circuit), noisy_counts)
