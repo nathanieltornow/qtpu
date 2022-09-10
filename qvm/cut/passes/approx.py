84fd8c97e811fc6689b8792211fc33c981a195a9
Nathaniel Tornow <nathaniel.tornow@tum.de>
2022-09-10 18:49:27 +0200

    add approximation pass

---
+++ qvm/cut/passes/approx.py
@@ -0,0 +1,36 @@
+from typing import Dict, Type
+
+from qiskit.dagcircuit import DAGCircuit
+from qiskit.transpiler.basepasses import TransformationPass
+
+from qvm.virtual_gate import (
+    VirtualBinaryGate,
+    VirtualCX,
+    VirtualCZ,
+    VirtualRZZ,
+    ApproxVirtualCX,
+    ApproxVirtualCZ,
+    ApproxVirtualRZZ,
+)
+
+STANDARD_APPROXIMATIONS = {
+    VirtualCZ: ApproxVirtualCZ,
+    VirtualCX: ApproxVirtualCX,
+    VirtualRZZ: ApproxVirtualRZZ,
+}
+
+
+class Approximation(TransformationPass):
+    def __init__(
+        self,
+        approx_gates: Dict[
+            Type[VirtualBinaryGate], Type[VirtualBinaryGate]
+        ] = STANDARD_APPROXIMATIONS,
+    ):
+        self.approx_gates = approx_gates
+
+    def run(self, dag: DAGCircuit) -> DAGCircuit:
+        for node in dag.op_nodes():
+            if type(node.op) in self.approx_gates:
+                approx_gate = self.approx_gates[type(node.op)](*node.op.params)
+                dag.substitute_node(node, approx_gate, inplace=True)
