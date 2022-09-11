f4efbcfeadc4daa1bb2f45af1ce10ec4310add5e
Nathaniel Tornow <nathaniel.tornow@tum.de>
2022-09-11 20:18:32 +0200

    add groups pass

---
+++ qvm/cut/passes/qubit_groups.py
@@ -0,0 +1,20 @@
+import itertools
+from typing import List, Set
+from qiskit.circuit import Qubit
+from qiskit.dagcircuit import DAGCircuit
+from qiskit.transpiler.basepasses import TransformationPass
+
+from qvm.cut.cut import cut_qubit_connection
+from qvm.converters import dag_to_connectivity_graph
+
+
+class QubitGroups(TransformationPass):
+    def __init__(self, groups: List[Set[Qubit]]):
+        assert len(groups) > 0
+        self.groups = groups
+        super().__init__()
+
+    def run(self, dag: DAGCircuit) -> DAGCircuit:
+        for group1, group2 in itertools.combinations(self.groups, 2):
+            for qubit1, qubit2 in itertools.product(group1, group2):
+                cut_qubit_connection(dag, qubit1, qubit2)
