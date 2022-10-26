fd57aa861e68aea4e46f8e660292700840d1d14c
Nathaniel Tornow <nathaniel.tornow@tum.de>
2022-10-26 14:25:31 +0200

    adding comments

--- examples/bisection.py
+++ examples/bisection.py
@@ -1,29 +1,33 @@
+from random import sample
 from qiskit import QuantumCircuit
 from qiskit_aer import AerSimulator
 
 from vqc import Knitter, cut, Bisection
-from vqc.knit.knit import _knit
+from vqc.bench.fidelity import fidelity
 
 if __name__ == "__main__":
-    # initialize a 4-qubit circuit
-    # circuit = QuantumCircuit.from_qasm_file("examples/qasm/circuit1.qasm")
-    circuit = QuantumCircuit(2)
-    circuit.h(0)
-    circuit.h(1)
-    circuit.cx(0, 1)
-    circuit.measure_all()
+    circuit = QuantumCircuit.from_qasm_file("examples/qasm/circuit1.qasm")
 
-    # build and run a transpiler using the bisection pass.
-
+    # Cut the circuit using the Bisection cutter
     virt_circ = cut(circuit, Bisection())
+    # print the virtual circuit, virtual gates are Barriers
     print(virt_circ)
 
-    knitter = Knitter(virt_circ)
-    print(knitter.samples())
+    # print both fragments as circuits
+    for fragment in virt_circ.fragments:
+        print(virt_circ.fragment_as_circuit(fragment))
 
+    # define a knitter the virtualization process
+    knitter = Knitter(virt_circ)
+    # get all circuit samples needed for the virtualization
+    # the samples are a dictionary with keys being the fragment ids and values being
+    # the circuits to execute for each fragment
+    samples: dict[str, list[QuantumCircuit]] = knitter.samples()
+
+    # execute the samples and store them accoring to the samples dictionary
+    # with the fragment ids as keys and the list of counts as values
     results = {}
-    for name, frag_circs in knitter.samples().items():
-        print("asdas", frag_circs)
+    for name, frag_circs in samples.items():
         results[name] = (
             AerSimulator()
             .run([circ.decompose() for circ in frag_circs])
@@ -31,19 +35,10 @@ if __name__ == "__main__":
             .get_counts()
         )
 
-    print(results)
-    res = knitter.knit(results)
+    # knit the results together to get the final probability distribution
+    prob_distr = knitter.knit(results)
 
-    # for sample in knitter.samples()[1]:
-    #     print(sample.decompose())
-
-    # dist_circ = VirtualCircuit.from_circuit(cut_circ)
-    # print(dist_circ)
-
-    # result = execute_virtual_circuit(dist_circ, 1000)
-    # print(result)
-
-    from vqc.bench.fidelity import fidelity
-
-    fid = fidelity(circuit, res.counts(10000))
+    # store the probability distribution as counts and get the hellinger fidelity
+    counts = prob_distr.counts(10000)
+    fid = fidelity(circuit, counts)
     print(fid)
