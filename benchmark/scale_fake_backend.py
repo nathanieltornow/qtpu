3c005ae5141403e7ac87dd2757ec7a52bbdb4a7a
Nathaniel Tornow <nathaniel.tornow@tum.de>
2023-03-27 22:38:15 +0200

    adding scale fake qpu

---
+++ benchmark/scale_fake_backend.py
@@ -0,0 +1,62 @@
+import csv
+import os
+import sys
+from time import perf_counter
+
+from fidelity import calc_fidelity
+from qiskit.circuit import QuantumCircuit
+from qiskit.providers.ibmq import AccountProvider, IBMQ
+from qiskit.providers.fake_provider import FakeMumbaiV2
+from qiskit_aer.noise import NoiseModel
+from qiskit.compiler import transpile
+
+from qvm.stack._types import QVMJobMetadata, QVMLayer
+
+
+FAKE_BACKEND = FakeMumbaiV2()
+
+BENCHNAME = "scale_mumbai_ghz"
+
+
+def benchmark_QVM_layer(qasms: list[str], provider: AccountProvider) -> None:
+    """Benchmark a QVM layer."""
+
+    RESULT_FILE_PATH = os.path.join(os.getcwd(), f"{BENCHNAME}.csv")
+
+    field_names = ["num_qubits", "fidelity"]
+    if not os.path.exists(RESULT_FILE_PATH):
+        os.makedirs(os.path.dirname(RESULT_FILE_PATH), exist_ok=True)
+        with open(RESULT_FILE_PATH, "w") as csv_file:
+            csv.DictWriter(csv_file, fieldnames=field_names).writeheader()
+
+    backend = provider.get_backend("ibmq_qasm_simulator")
+
+    for qasm in qasms:
+        qasm_file = os.path.join(os.path.dirname(__file__), qasm)
+
+        circuit = QuantumCircuit.from_qasm_file(qasm_file)
+        t_circuit = transpile(circuit, backend=FAKE_BACKEND, optimization_level=3)
+
+        results = backend.run(
+            t_circuit, shots=100000, noise_model=NoiseModel.from_backend(FAKE_BACKEND)
+        ).result()
+
+        fid = calc_fidelity(circuit, results.get_counts(), provider)
+
+        with open(RESULT_FILE_PATH, "a") as csv_file:
+            csv.DictWriter(csv_file, fieldnames=field_names).writerow(
+                {
+                    field_names[0]: circuit.num_qubits,
+                    field_names[1]: fid,
+                }
+            )
+
+
+def main():
+    qasms = [f"ghz/{i}.qasm" for i in [4, 6, 10, 14, 18, 22, 26]] * 4
+    provider = IBMQ.load_account()
+    benchmark_QVM_layer(qasms, provider)
+
+
+if __name__ == "__main__":
+    main()
