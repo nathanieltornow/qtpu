5f5337a7434d6fc33bfa30eeccf146984bb710ea
Nathaniel Tornow <nathaniel.tornow@tum.de>
2023-04-26 21:39:17 +0200

    add optimal wire cutting

--- qvm/cutting/wire_cutting.py
+++ qvm/cutting/wire_cutting.py
@@ -1,7 +1,6 @@
 from qiskit.circuit import QuantumCircuit, Qubit, QuantumRegister
 
-from qvm.util import fold_circuit, unfold_circuit, decompose_qubits
-from qvm.virtual_gates import WireCut, VirtualSWAP
+from .optimal import cut_optimal
 
 
 def cut_wires_optimal(
@@ -10,44 +9,10 @@ def cut_wires_optimal(
     max_cuts: int = 4,
     max_fragment_size: int | None = None,
 ) -> QuantumCircuit:
-    pass
-#     from clingo.control import Control
-#     import importlib.resources
-
-#     two_qubit_circ, one_qubit_circs = fold_circuit(circuit)
-
-#     asp = _circuit_to_asp(two_qubit_circ)
-
-#     with importlib.resources.path("qvm", "asp") as path:
-#         asp_file = path / "dag_partition.lp"
-#         asp += asp_file.read_text()
-        
-#     asp += f"#const num_fragments = {num_fragments}.\n"
-
-#     control = Control()
-#     control.configuration.solve.models = 0  # type: ignore
-#     control.add("base", [], asp)
-#     control.ground([("base", [])])
-#     solve_result = control.solve(yield_=True)  # type: ignore
-#     opt_model = None
-#     for model in solve_result:  # type: ignore
-#         opt_model = model
-#         print(model)
-
-#     if opt_model is None:
-#         raise ValueError("No solution found.")
-#     print(opt_model)
-
-    # qubits_sets: list[set[Qubit]] = [set() for _ in range(num_fragments)]
-    # for symbol in opt_model.symbols(shown=True):
-    #     if symbol != "partition" and len(symbol.arguments) != 2:
-    #         continue
-    #     qubit_idx, partition = (
-    #         symbol.arguments[0].number,
-    #         symbol.arguments[1].number,
-    #     )
-    #     qubits_sets[partition].add(circuit.qubits[qubit_idx])
-
-    # return decompose_qubits(circuit, qubits_sets)
-
-
+    return cut_optimal(
+        circuit,
+        num_fragments=num_fragments,
+        max_wire_cuts=max_cuts,
+        max_gate_cuts=0,
+        max_fragment_size=max_fragment_size,
+    )
