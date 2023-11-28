fd4d6e7c57628f89eb8ac02b34f82a320a4cc586
Nathaniel Tornow <nathaniel.tornow@tum.de>
2023-11-28 19:31:56 +0100

    add qvm compiler import

--- qvm/__init__.py
+++ qvm/__init__.py
@@ -1,4 +1,4 @@
-from .compiler import CutterCompiler, StandardQVMCompiler
+from .compiler import CutterCompiler, StandardQVMCompiler, QVMCompiler
 from .quasi_distr import QuasiDistr
 from .runtime.runner import run
 from .virtual_circuit import FragmentMetadata, VirtualCircuit
