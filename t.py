6e79f3dd6a30af447180ba05d89075f6c775ebdc
Nathaniel Tornow <nathaniel.tornow@tum.de>
2023-12-16 10:05:52 +0100

    exp

--- t.py
+++ t.py
@@ -1,15 +1,34 @@
-from pytket import Circuit
-from pytket.pauli import Pauli, QubitPauliString
+from pytket.passes import DecomposeMultiQubitsCX
+from pytket.circuit import Circuit
 
 
-c = Circuit()
-q_reg = c.add_q_register("a", 1)
-c.Sdg(q_reg[0])
-# c.H(0, condition_bits=[0], condition_value=1)
+pass1 = DecomposeMultiQubitsCX()
+
+circ = Circuit(3)
+circ.CRz(0.5, 0, 1)
+circ.T(2)
+circ.add_barrier([0, 1, 2], data="v_cx")
+circ.CSWAP(2, 0, 1)
 
 
-ps = QubitPauliString(c.qubits[0], Pauli.Z)
-print(c.qubits)
-p = Pauli(1)
-print(p)
-print(c.get_commands())
+
+for com in circ.get_commands():
+    if com.op.get_name() == "Barrier":
+        print(com.op.data)
+    print(com.op)
+
+# from pytket.predicates import CompilationUnit
+# from pytket.predicates import GateSetPredicate
+# from pytket.circuit import OpType
+
+# pred1 = GateSetPredicate({OpType.Rz, OpType.X, OpType.CX})
+# cu = CompilationUnit(circ, [pred1])
+# pass1.apply(cu)
+# pass1.apply(cu)
+# circ1 = cu.circuit
+
+
+# print(circ1.get_commands())
+# print(cu.check_all_predicates())
+# pass1.apply(cu)
+# print(cu.check_all_predicates())
