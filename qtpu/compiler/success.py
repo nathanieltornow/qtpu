a93b9294b615c1ff496d34e09c549499b487fad9
Nathaniel Tornow <nathaniel.tornow@tum.de>
2024-06-16 08:29:09 +0000

    adding new success functions

--- qtpu/compiler/success.py
+++ qtpu/compiler/success.py
@@ -5,3 +5,11 @@ from qtpu.compiler.util import get_leafs
 
 def success_probability_static(ir: CompressedIR, tree: ctg.ContractionTree) -> float:
     return min((1 - 1e-4) ** len(ir.decompress_nodes(leaf)) for leaf in get_leafs(tree))
+
+
+def success_num_nodes(ir: CompressedIR, tree: ctg.ContractionTree) -> int:
+    return -max(len(ir.decompress_nodes(leaf)) for leaf in get_leafs(tree))
+
+
+def success_num_qubits(ir: CompressedIR, tree: ctg.ContractionTree) -> int:
+    return -max(len(ir.num_qubits(leaf)) for leaf in get_leafs(tree))
